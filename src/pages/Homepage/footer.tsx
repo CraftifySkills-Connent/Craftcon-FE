c4adca858e2f756098e97a21619cacb00061ed33
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-06 15:28:08 +0100

    update: development

--- src/pages/Homepage/footer.tsx
+++ src/pages/Homepage/footer.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { ReactComponent as CraftconLogo } from "../../assets/images/craftcon.svg";
+import { Link } from "react-router-dom";
+
+const navItems = [
+  { title: "Home", link: "" },
+  { title: "Services", link: "" },
+  { title: "About Us", link: "" },
+  { title: "Professionals", link: "" },
+];
 
 const Footer: React.FC = () => {
   return (
-    <div className="mt-[86px] px-[88px] border-gray-200 bg-light pt-[50px]">
-      <div className="grid items-center justify-center pb-[46px]">
+    <div className="relative mt-[86px] px-[88px] bg-light pt-[50px] pb-[50px]">
+      <div className="grid items-center justify-center pb-[123px]">
         <div className="flex justify-center">
           <CraftconLogo className="mb-[20px]" />
         </div>
@@ -12,6 +20,16 @@ const Footer: React.FC = () => {
           Get top rated service providers give you a solution to your problems
         </h5>
       </div>
+      <div className="flex justify-center items-center gap-[50px]">
+        {navItems?.map((item) => (
+          <div key={item?.title} className="">
+            <Link to={item?.link} className="text-gray">
+              {item?.title}
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div className="absolute bottom-0 left-0 w-full h-[10px] bg-primary"></div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,13 @@ export default {
   theme: {
     extend: {
       colors: {
+        light: "#F4F9FF",
         primary: "#0158D8",
         secondary: "#1C1C1C",
         tertiary: "#686868",
         gray: "#475467",
         ashen: "#D0D0D0",
-        light: "#F4F9FF",
+        default: "#016FED",
       },
       boxShadow: {
         "service-box": "0px 6.79px 19.23px -5.65px rgba(58, 77, 233, 0.15)",
