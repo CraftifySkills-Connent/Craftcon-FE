69ec3177207067a527d261865e6ca9ee31ddb6d9
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-24 11:30:25 +0100

    update: developement

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -61,45 +61,50 @@ const Navbar: React.FC = () => {
           )}
         </button>
       </div>
-      <div
-        className={`fixed top-0 left-0 right-0 bg-white z-40 flex flex-col items-center justify-center transition-transform duration-300 transform ${
-          isMenuOpen ? "translate-y-0" : "-translate-y-full"
-        } md:hidden h-screen`}
-      >
-        <div className="flex flex-col items-center justify-center space-y-6 mt-40">
-          {navItems?.map((item) => (
-            <Link
-              key={item?.title}
-              to={item?.link}
-              className="text-gray-700 hover:text-primary text-xl py-4"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              {item?.title}
-            </Link>
-          ))}
+      {/* Overlay for Full-screen Background */}
+      {isMenuOpen && (
+        <div className="fixed top-0 left-0 right-0 bottom-0 bg-white z-40 md:hidden">
+          <div
+            className={`flex flex-col items-center justify-center transition-transform duration-300 transform ${
+              isMenuOpen ? "translate-y-0" : "-translate-y-full"
+            }`}
+          >
+            <div className="flex flex-col items-center justify-center space-y-8 mt-40">
+              {navItems?.map((item) => (
+                <Link
+                  key={item?.title}
+                  to={item?.link}
+                  className="text-gray-700 hover:text-primary text-xl py-4"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  {item?.title}
+                </Link>
+              ))}
+            </div>
+            <div className="flex flex-col gap-4 mt-8 w-full px-6">
+              <CustomButton
+                variant="outlined"
+                onClick={() => {
+                  navigate("/login");
+                  setIsMenuOpen(false);
+                }}
+                buttonText="Sign in"
+                className="w-full h-12"
+              />
+              <CustomButton
+                variant="contained"
+                onClick={() => {
+                  navigate("/signup");
+                  setIsMenuOpen(false);
+                }}
+                disableElevation
+                buttonText="Sign up"
+                className="w-full h-12"
+              />
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 mt-40 w-full px-6">
-          <CustomButton
-            variant="outlined"
-            onClick={() => {
-              navigate("/login");
-              setIsMenuOpen(false);
-            }}
-            buttonText="Sign in"
-            className="w-full h-12"
-          />
-          <CustomButton
-            variant="contained"
-            onClick={() => {
-              navigate("/signup");
-              setIsMenuOpen(false);
-            }}
-            disableElevation
-            buttonText="Sign up"
-            className="w-full h-12"
-          />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
