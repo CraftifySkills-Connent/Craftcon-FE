b40664d0411700d960ba5ce9554295574ff0404e
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-24 16:36:34 +0100

    update: development

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { ReactComponent as Logo } from "../src/assets/img/logo.svg";
 
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -10,7 +11,13 @@ import { router } from "./routes/index.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen">
+            <Logo className="w-24 h-24" />
+          </div>
+        }
+      >
         <RouterProvider router={router} />
       </Suspense>
     </Provider>
